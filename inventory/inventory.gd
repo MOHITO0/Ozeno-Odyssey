e1e948c1ca26a59d29c5862d5e3f24899992df53
MOHITO0 <122308850+MOHITO0@users.noreply.github.com>
2024-05-02 20:19:58 +0300

    Update 0.1.11. Добавлены предметы. Предметы стакаются.

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -7,9 +7,14 @@ signal updated
 @export var slots: Array[InventorySlot]
 
 func insert(item: InventoryItem):
-	for i in range(slots.size()):
-		if !slots[i].item:
-			slots[i].item = item
-			break
+	var itemSlots = slots.filter(func(slot): return slot.item == item)
+	if !itemSlots.is_empty():
+		itemSlots[0].amount += 1
+	else:
+		var emptySlots = slots.filter(func(slot): return slot.item == null)
+		if !emptySlots.is_empty():
+			emptySlots[0].item = item
+			emptySlots[0].amount = 1	
 			
+
 	updated.emit()
